80bb344fd02b1684c00ec2e3f2185a85838d4ec3
Gabriel Redondo <gdredfield046@gmail.com>
2022-04-01 10:31:09 +0800

    Latest version update with license management

--- todolist.php
+++ todolist.php
@@ -18,31 +18,33 @@ if ( ! defined( 'ABSPATH' ) ) {
   die; // Exit if accessed directly.
 }
 
-function todolist_elementor_addon() {
- 
-  // Load plugin file
-  require_once( __DIR__ . '/trunk/plugin.php' );
-  // Run the plugin
-  \Todolist_Elementor_Addon\Plugin::instance();
-
+function require_activation(){
+  add_menu_page('Todolist', 'Todolist', 'manage_options' ,__FILE__, 'license_credentials_page', 'dashicons-clipboard');
 }
-add_action( 'plugins_loaded', 'todolist_elementor_addon' );
-
-//adding plugin to admin menu
-add_action('admin_menu', 'addAdminPageContent');
 
 function addAdminPageContent() {
   add_menu_page('Todolist', 'Todolist', 'manage_options' ,__FILE__, 'crudAdminPage', 'dashicons-clipboard');
   add_submenu_page(__FILE__, 'Button Tracker', 'Button Tracker', 'manage_options' , 'button_tracker_tab', 'button_tracker_list_tab');
 }
 
+//bind elementor plugin compatibility check and initiator to load
+add_action( 'plugins_loaded', 'todolist_elementor_addon' );
+function todolist_elementor_addon() {
+  // Load plugin file
+  require_once( __DIR__ . '/trunk/plugin.php' );
+  // Run the plugin
+  \Todolist_Elementor_Addon\Plugin::instance();
+}
+
 //file inclusions for main features
-require_once( __DIR__ . '/trunk/activation.php' );
 require_once( __DIR__ . '/trunk/wordpressfunctions.php' );
 require_once( __DIR__ . '/trunk/elementor-sub-functions.php' );
 require_once( __DIR__ . '/trunk/btn-click-tracker.php' );
 require_once( __DIR__ . '/trunk/admin-todolist-page.php');
 require_once( __DIR__ . '/trunk/admin-btn-tracker-tab.php');
+require_once( __DIR__ . '/trunk/activation.php' );
+require_once( __DIR__ . '/trunk/license-validation.php');
+
 
 //send existing btn tracker count to laravel API
 register_activation_hook( __FILE__, 'send_btn_count');
@@ -54,5 +56,12 @@ register_activation_hook( __FILE__, 'crudOperationsTable');
 register_activation_hook( __FILE__, 'btnCountOperationsTable');
 //register function to activate database initialization for button tracking list operations table
 register_activation_hook( __FILE__, 'btnCountlist');
+//main activation functions with license check
+register_activation_hook( __FILE__, 'registerDomain');
 
-
+ //adding plugin to admin menu
+ if(license_validation()){
+  add_action('admin_menu', 'addAdminPageContent');
+}else{
+  add_action('admin_menu', 'require_activation');
+}
--- trunk/activation.php
+++ trunk/activation.php
@@ -81,3 +81,46 @@ function btnCountlist(){
       dbDelta($sql);
     }
 }
+
+//register new domain to laravel api and database table creation for license check
+function registerDomain(){
+  global $wpdb;
+  $base_url = get_site_url();
+  $charset_collate = $wpdb->get_charset_collate();
+  date_default_timezone_set('Asia/Bangkok');
+  $date = date('Y-m-d h:i:s');
+  $table_name = $wpdb->prefix . 'license_check';
+  $license_key_initial = 'N/A';
+  $sql = "CREATE TABLE $table_name (
+    `id` int(50) NOT NULL AUTO_INCREMENT,
+    `base_url` varchar(100) DEFAULT NULL,
+    `license_key`  varchar(50) DEFAULT NULL,
+    `date_added`  datetime,
+    PRIMARY KEY(id)
+    );";
+  $sql2 = "INSERT INTO $table_name(base_url, license_key, date_added) VALUES('$base_url','$license_key_initial','$date');";
+
+  if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+    dbDelta($sql2);
+  }
+
+  $status = 'Deactivated';
+  $license_key = implode( '-', str_split( substr( strtoupper( md5( time() . rand( 1000, 9999 ) ) ), 0, 20 ), 4 ) );
+  $send_domain_tb = array(
+    'site_url' => $base_url,
+    'date_added' => $date,
+    'status' => $status,
+    'license_key' => $license_key,
+  );
+  $data_push_to_api = json_encode($send_domain_tb);
+  $send_domain = wp_remote_post('https://dashboard.sg-webdesign.net/registerurl', array(
+    'method' => 'POST',
+    'headers' => array(
+      'Content-Type' => 'application/json'
+    ),
+      'sslverify' => false,
+      'body' => $data_push_to_api,
+    )); 
+}
